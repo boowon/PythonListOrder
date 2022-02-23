888a6fcca45e23e061e3d4c8b9e6a19d66489f76
boowon <94376439+boowon@users.noreply.github.com>
2022-02-23 18:08:45 +0100

    Update oderlist.py

--- oderlist.py
+++ oderlist.py
@@ -1,15 +1,26 @@
 l=[]
+<<<<<<< Updated upstream
 nbelt=input("give number of numbers you will put\n")
+=======
+list=[]
+nbelt=input("give number of number you will put\n")
+>>>>>>> Stashed changes
 for i in range(int(nbelt)):
     nbr=input("give your number\n")
     l.append(int(nbr))
+
 print("the list given",l)
-for i in range(len(l)-1):
+
+for i in range(0,len(l)-1):
     for j in range(i+1,len(l)):
-        if(l[i] >= l[j]):
-            temp=l[j]
-            l[j]=l[i]
-            l[i]=temp
+        if(l[i] > l[j]):
+           temp=l[j]
+           l[j]=l[i]
+           l[i]=temp
 
-print("the list in order",l)            
-
+list.append(l[0])
+for i in range(1,len(l)):
+    if(l[i]!=l[i-1]):
+        list.append(l[i])
+           
+print("the list in order",list) 
